72ea5f1efec7d05c4531a917f583768b982f2b0b
dominicwild <dominicwild@dominicwild.com>
2023-01-03 12:55:03 +0000

    Finish counting DNA nucleotides exercise

--- Counting_DNA_Nucleotides/main.py
+++ Counting_DNA_Nucleotides/main.py
@@ -1,3 +1,16 @@
 def count_dna_nucleotides(dna_sequence: str) -> str:
     number_of_a = dna_sequence.count("A")
-    return f'{number_of_a} 0 0 0'
+    number_of_c = dna_sequence.count("C")
+    number_of_g = dna_sequence.count("G")
+    number_of_t = dna_sequence.count("T")
+    return f'{number_of_a} {number_of_c} {number_of_g} {number_of_t}'
+
+
+def main():
+    with open("sequences.txt") as sequences:
+        for sequence in sequences:
+            print(count_dna_nucleotides(sequence))
+
+
+if __name__ == '__main__':
+    main()
--- Counting_DNA_Nucleotides/tests/test_main.py
+++ Counting_DNA_Nucleotides/tests/test_main.py
@@ -5,6 +5,13 @@ from Counting_DNA_Nucleotides.main import count_dna_nucleotides
 
 @pytest.mark.parametrize("nucleotide_seq, expected_output", [
     ("AAA", "3 0 0 0"),
+    ("CCC", "0 3 0 0"),
+    ("GGG", "0 0 3 0"),
+    ("TTT", "0 0 0 3"),
+    ("ACGT", "1 1 1 1"),
+    ("ACCGGGTTTT", "1 2 3 4"),
+    ("ACAGGTACT", "3 2 2 2"),
+    ("AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC", "20 12 17 21"),
 ])
 def test_count_number_of_nucleotides(nucleotide_seq, expected_output):
     assert count_dna_nucleotides(nucleotide_seq) == expected_output
